9179c1b9b2d2b7d4dfaf64adc4e1d66e44642c9b
Sobhan ghasemi <mohammadghasemi3033@gmail.com>
2024-10-12 18:22:23 +0330

    feat : creating the snake character and the apple

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ let canvas , ctx
 canvas = document.getElementById("canvas")
 ctx = canvas.getContext("2d")
 
-console.log(ctx)
+//creating the Snake
+ctx.fillStyle = "#084646"
+ctx.fillRect(20 , 20 , 100 , 15)
+
+
+//creating the apple
+ctx.fillStyle = "#8B080D"
+ctx.fillRect(500 , 100 , 15 , 15)
