f1869f270d33ed85abbb52cfc06df75768e7535b
Sobhan ghasemi <mohammadghasemi3033@gmail.com>
2024-10-13 18:23:41 +0330

    Feat : creating some functions to make the snakes movement

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-let canvas , ctx
-canvas = document.getElementById("canvas")
-ctx = canvas.getContext("2d")
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
 
-//creating the Snake
-ctx.fillStyle = "#084646"
-ctx.fillRect(20 , 20 , 100 , 15)
+let scale = 15;
 
 
 //creating the apple
 ctx.fillStyle = "#8B080D"
 ctx.fillRect(500 , 100 , 15 , 15)
+
+function Snake(){
+    this.x = 10
+    this.y = 10
+    this.xSpeed = 10
+    this.ySpeed = 0
+
+    this.snakeDraw = function () {
+        ctx.fillStyle = "#084646"
+        ctx.fillRect(this.x, this.y, scale, scale);
+    };
+    this.updateLocation = function () {
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+    };
+}
+
+window.addEventListener("load",()=>{
+let snake = new Snake();
+setInterval(()=>{
+    ctx.clearRect(0 , 0 , canvas.width , canvas.height)
+    snake.snakeDraw();
+    snake.updateLocation()
+    },100)
+})
