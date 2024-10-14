26a817738f550b97af595e7c01c1156496d47329
Sobhan ghasemi <mohammadghasemi3033@gmail.com>
2024-10-14 16:33:18 +0330

    debuging the bug of showing the apple and resizing canvas

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ function Apple() {
     this.generateLocation = function(snake) {
         let isOnSnake;
         do {
-            this.x = (Math.floor(Math.random() * rows)) * scale;
-            this.y = (Math.floor(Math.random() * columns)) * scale;
-            isOnSnake = snake.body.some(part => part.x === this.x && part.y === this.y);
+            this.x = Math.floor(Math.random() * columns) * scale;
+            this.y = Math.floor(Math.random() * rows) * scale;
+            isOnSnake = (snake.x === this.x && snake.y === this.y);
         } while (isOnSnake);
-    };
+    }
+
     this.appleDraw = function() {
         ctx.fillStyle = "#8B080D";
         ctx.fillRect(this.x, this.y, scale, scale);
@@ -21,53 +22,65 @@ function Apple() {
 }
 
 function Snake() {
-    this.body = [{ x: 10, y: 10 }];
+    this.x = 10;
+    this.y = 10;
     this.xSpeed = scale;
     this.ySpeed = 0;
 
     this.snakeDraw = function() {
         ctx.fillStyle = "#084646";
-        this.body.forEach(part => ctx.fillRect(part.x, part.y, scale, scale));
+        ctx.fillRect(this.x, this.y, scale, scale);
     };
 
     this.updateLocation = function() {
-        const head = { x: this.body[0].x + this.xSpeed, y: this.body[0].y + this.ySpeed };
-
-        // Check for wall collisions
-        if (head.x >= canvas.width) head.x = 0;
-        if (head.y >= canvas.height) head.y = 0;
-        if (head.x < 0) head.x = canvas.width - scale;
-        if (head.y < 0) head.y = canvas.height - scale;
-
-        this.body.unshift(head); // Add new head
-        this.body.pop(); // Remove the tail
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+        if (this.x >= canvas.width) {
+            this.x = 0;
+        }
+        if (this.y >= canvas.height) {
+            this.y = 0;
+        }
+        if (this.x < 0) {
+            this.x = canvas.width - scale;
+        }
+        if (this.y < 0) {
+            this.y = canvas.height - scale;
+        }
     };
 
     this.updateDirection = function(key) {
         switch (key) {
             case "ArrowUp":
-                if (this.ySpeed === 0) { this.xSpeed = 0; this.ySpeed = -scale; }
+                if (this.ySpeed === 0) {
+                    this.xSpeed = 0;
+                    this.ySpeed = -scale;
+                }
                 break;
             case "ArrowLeft":
-                if (this.xSpeed === 0) { this.xSpeed = -scale; this.ySpeed = 0; }
+                if (this.xSpeed === 0) {
+                    this.xSpeed = -scale;
+                    this.ySpeed = 0;
+                }
                 break;
             case "ArrowRight":
-                if (this.xSpeed === 0) { this.xSpeed = scale; this.ySpeed = 0; }
+                if (this.xSpeed === 0) {
+                    this.xSpeed = scale;
+                    this.ySpeed = 0;
+                }
                 break;
             case "ArrowDown":
-                if (this.ySpeed === 0) { this.xSpeed = 0; this.ySpeed = scale; }
+                if (this.ySpeed === 0) {
+                    this.xSpeed = 0;
+                    this.ySpeed = scale;
+                }
                 break;
         }
-    };
+    }
 
     this.hasEatenFood = function(food) {
-        return this.body[0].x === food.x && this.body[0].y === food.y;
-    };
-
-    this.grow = function() {
-        // Add a new segment to the snake's body
-        this.body.push({}); // Push an empty object which will be updated later
-    };
+        return this.x === food.x && this.y === food.y;
+    }
 }
 
 window.addEventListener("load", () => {
@@ -80,10 +93,8 @@ window.addEventListener("load", () => {
         snake.snakeDraw();
         apple.appleDraw();
         snake.updateLocation();
-
         if (snake.hasEatenFood(apple)) {
             apple.generateLocation(snake);
-            snake.grow(); // Grow the snake when it eats food
             console.log(apple.x + " " + apple.y);
         }
     }, 60);
